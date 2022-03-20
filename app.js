bc28ef14841645c849ec7468a9771a40afd28355
Charlie Hölscher <charlieholscher@gmail.com>
2022-03-20 17:20:44 +0100

    added a journey route from the profile page and did some basic styling of the profile page.

--- app.js
+++ app.js
@@ -37,6 +37,10 @@ app.use(login);
 const profilepage = require("./routes/profilepage");
 app.use(profilepage);
 
+
+const journeypage = require("./routes/journey.routes");
+app.use(journeypage);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
